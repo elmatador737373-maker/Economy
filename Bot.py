a0a22378cd0b8ec966c34659e9d5a63f329c162c
elmatador737373-maker <elmatador737373@gmail.com>
2026-03-03 14:59:19 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -5,6 +5,21 @@ import sqlite3
 import random
 import asyncio
 import os
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot online!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+Thread(target=run).start()
 
 TOKEN = os.environ["TOKEN"]
 
