d2df073c183a46209f19c184639069c6cffe84af
elmatador737373-maker <elmatador737373@gmail.com>
2026-08-10 01:35:43 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -64,7 +64,7 @@ TICKET_IDS = {
 VERIFIED_ROLE_IDS = [0]  
 
 # Nomi dei file locali presenti nella stessa cartella del bot
-NOME_FILE_BANNER = "giphy.gif"
+NOME_FILE_BANNER = "399B3005-E3EB-4438-A080-7079F9F8E462.png"
 NOME_FILE_BANNER_VERIFICA = "298087E6-C4DB-42C8-82E9-3A637AD0E4DA.png"
 
 TZ_ZONA = pytz.timezone("Europe/Rome")
